0e76afb8beae1c0a715ac2ac594dfccf9ba5fd74
BiHi <40806031+DanilSpakov@users.noreply.github.com>
2022-01-24 19:56:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="page__wrapper">
+		<div class="wrapper">
+			<header>
+				<div class="header__l"><img src="img/logo.svg" alt=""></div>
+				<div class="header__r"><a href="#">Войти</a></div>
+			</header>
+
+			<div class="container">
+				<div class="container__desc">
+					<h1>Прийми участие в розыграшах</h1>
+					<p>и получи шанс выиграть крутые аккаунты в Brawl Stars</p>
+				</div>
+
+				<div class="container__giveaway">
+					<div class="giveaway__block">
+						<div class="giveaway__info">
+							<div class="info__l">
+								<a href="#">#: QLJ02CR <img src="img/share.svg" alt=""></a>
+								<!-- <img src="img/share.svg" alt=""> -->
+							</div>
+							<div class="info__r">
+								<p>Конец через:  <span> 1d 10h</span></p>
+								<img src="img/info.svg" alt="">
+							</div>
+						</div>
+						<div class="giveawey__name">
+							<p>Аккаунт с леоном</p>
+						</div>
+						<div class="giveawey__image">
+							<img src="img/test__image.png" alt="image">
+						</div>
+						<div class="giveawey__members">
+							<div class="member__l"><p>Участников</p></div>
+							<div class="member__r"><p>0</p></div>
+						</div>
+						<div class="giveawey__claim">
+							<a href="#">Войдите чтобы принять участие</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="container__winners">
+					<div class="winners__block">
+						<div class="winners__title">
+							<p>Таблица победителей</p>
+						</div>
+
+						<div class="winners__users">
+							<p>1.</p>
+						</div>
+						<div class="winners__users">
+							<p>2.</p>
+						</div>
+						<div class="winners__users">
+							<p>3.</p>
+						</div>
+						<div class="winners__users">
+							<p>4.</p>
+						</div>
+						<div class="winners__users">
+							<p>5.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="footer">
+				<p>brawlquest - 2022</p>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
